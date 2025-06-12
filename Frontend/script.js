f83bf56788dcbbdabb387854f4b56da68da267ae
fizasiddiqui <fouziasiddiqui8298@gmail.com>
2025-06-12 23:29:01 +0530

    index.html

---
+++ Frontend/script.js
@@ -0,0 +1,10 @@
+var h1 = React.createElement('h1', null,"Hello from  React ")
+
+var parent = document.querySelector("#parent")
+
+console.log(parent)
+
+ var root = ReactDOM.createRoot(parent) // ReactDOM ais likenge tab sahi hoga warna galt hoga..
+
+ root.render(h1)
+
